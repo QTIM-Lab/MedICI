1886d4346d5e31ad18fff36e222219c4a5ea84d4
Benjamin Bearce <bbearce@gmail.com>
2020-11-11 23:23:49 -0500

    Update read_files.py

--- Docker_Submissions/read_files.py
+++ Docker_Submissions/read_files.py
@@ -3,10 +3,6 @@ import os, pandas as pd, random, sys
 # Sample Deep Learning Run
 import sample_deep_learning_model
 
-# for local development
-#IN = 'directory_of_files'
-#OUT = 'output_dir'
-
 # for use with docker mounts
 IN = '/mnt/in'
 OUT = '/mnt/out'
@@ -19,10 +15,13 @@ if __name__ == "__main__":
     classes = ['A','O','G']
     submission_csv = pd.DataFrame({"CPM_RadPath_2020_ID":files, "class":[random.choice(classes) for file in files]})
     
-    # Deep Learning Model
-    sample_deep_learning_model.run()
-    # Deep Learning Model
+    # Deep Learning Model - local
+    # sample_deep_learning_model.run(IN+"/data", OUT)
+    # Deep Learning Model - local
+
+    # Deep Learning Model - on submission
+    sample_deep_learning_model.run("/workspace/data", OUT)
+    # Deep Learning Model - on submission
 
     submission_csv.to_csv(os.path.join(OUT,"random.csv"), index=None)
-
 
